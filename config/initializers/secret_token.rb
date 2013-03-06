b5e667d2ffc9008ba6c2fcb50450aaaa8c2ee40e
Brad Smith <bradsmithinc@gmail.com>
2013-03-06 11:51:23 -0500

    inital

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Instalodger::Application.config.secret_token = '1937c2873dce1f0e2c3282b5c7493007fdcc7df721631f231e52669384b3ab5911a06c1f56fd91918ce4686cae13ba0a25521f33217740cde4d1ddb0dcf85dfb'
